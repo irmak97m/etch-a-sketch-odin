97266027e9b799be5d44fb2ac137d35a8c3f7d11
irmak.97m <218309191+irmak97m@users.noreply.github.com>
2025-07-15 03:29:43 +0300

    mouse actions and set gridsize button animation left

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ const DRAWING_SIZE = 480;
 const DRAWING_AREA = document.querySelector("#drawing");
 
 const colorPicker = document.querySelector("#colorPick");
-const quickColorPicker = document.querySelectorAll("ul li");
+const quickColorPicker = document.querySelectorAll("li");
 
 const setGridBtn = document.querySelector("#rangeSet button");
 const rangeBar = document.querySelector("#rangeSet input");
+rangeBar.value = 30;
 let gridText = document.querySelector("#rangeSet span");
 
 const clearBtn = document.querySelector("#clearButton");
@@ -18,7 +19,6 @@ let gridCount= 30;
 let divSize = 16;
 let divCount = 900;
 let colorVal;
-
 divCreate();
 
 function divCreate() {
@@ -35,7 +35,6 @@ function divRemove() {
     document.querySelectorAll(".draw").forEach(div => div.remove());
 };
 
-
 rangeBar.addEventListener("change", () => {
     gridText.textContent = `${rangeBar.value} x ${rangeBar.value} Grid`;
     // setGridBtn animation
@@ -47,28 +46,42 @@ setGridBtn.addEventListener("click", () => {
     divSize = DRAWING_SIZE / gridCount;
     divCount = Math.pow(gridCount,2);
     divCreate();
-    // message content 
+    if (colorVal !== undefined) {
+        message.textContent = `Grid size set to ${gridCount} x ${gridCount}`;
+    } else {
+        message.textContent = `Please pick a color to start`;
+    }
 });
 
 clearBtn.addEventListener("click",() => {
     divRemove();
     divCreate();
+    colorVal = undefined;
+    quickColorPicker.forEach(picker => {
+        picker.style.outline = "none";
+    });
+    colorPicker.style.outline = "none";
+    message.textContent = "Drawing area cleared";
 });
 
 colorPicker.addEventListener("change", () => {
+    colorPicker.style.outline = "2px solid black";
     colorVal = colorPicker.value;
-    // mark color picker
-    // unmark all quick color pickers
+    message.textContent = `Your color set to ${colorVal}`;
+    quickColorPicker.forEach(picker => {
+        picker.style.outline = "none";
+    });
 });
-
 
 quickColorPicker.forEach(picker => {
    picker.addEventListener("click", () => {
-        // colorVal cannot be set, because i can't get backgroundcolor val
-        colorVal = picker.style["background-color"];
-        console.log(colorVal);
-        // mark only selected quick color
-        // unmark rest of the quick color and colorpicker
+        quickColorPicker.forEach(picker => {
+            picker.style.outline = "none";
+        });
+        colorVal = getComputedStyle(picker).backgroundColor;
+        picker.style.outline = "2px solid black";
+        colorPicker.style.outline = "none";
+        message.textContent = `Your color set to ${picker.id}`;
    });
 });
 
